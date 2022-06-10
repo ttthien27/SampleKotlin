138d4b054f45962207823c1ed8e235a9fc41a558
ttthien27 <ttthien.20@gmail.com>
2022-06-10 18:30:46 +0700

    layout sign in and sign up

--- app/src/main/java/com/example/kotlinsample/view/activity/SignInActivity.kt
+++ app/src/main/java/com/example/kotlinsample/view/activity/SignInActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinsample.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinsample.R
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
